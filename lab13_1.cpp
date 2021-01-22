bc7e38f3bec19d0cf8ad4e692c63c9f363365cd0
DifficultIV <kitpisan@gmail.com>
2021-01-22 17:37:38 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[],int N){  
+    int j;  
+    for (int i = 1; i < N; i++) 
+    {  
+        double temp = d[i];  
+        j = i - 1;  
+  
 
-}
+        while (j >= 0 && d[j] < temp) 
+        {  
+            d[j + 1] = d[j];  
+            j--;  
+        }  
+        d[j + 1] = temp;  
+		cout << "Pass " << i << ":";
+		for(int i = 0; i < 10; i++) cout << d[i] << " ";
+		cout << endl;
+    }  
+    }  
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
